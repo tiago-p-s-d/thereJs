19d1bd71c0578afdebffa2c668402c45ce37926d
tiago-p-s-d <tiagopsilvad@gmail.com>
2023-10-08 16:38:01 -0300

    foram mudados uns valores, começar a deixar mais prático para o trabalho

--- js/main.js
+++ js/main.js
@@ -5,18 +5,20 @@ import * as THREE from './three.module.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
+
+const canvasContainer = document.getElementById('canvas-container');
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize( 500, 500 );
+canvasContainer.appendChild(renderer.domElement);
 
 //adicionando o cubo:
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const geometry = new THREE.BoxGeometry( 0.5, 0.5, 0.5 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff330 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 5;
+camera.position.z = 2;
 
 //começando a animar:
 
