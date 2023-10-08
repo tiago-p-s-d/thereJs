b485883abfa0d2c7365b42d0e10e904451b0f168
tiago-p-s-d <tiagopsilvad@gmail.com>
2023-10-07 23:56:53 -0300

    tirei ?v=f3c9e33d ao caminho

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from './node_modules/three/build/three.module.js?v=f3c9e33d';
+import * as THREE from './node_modules/three/build/three.module.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
