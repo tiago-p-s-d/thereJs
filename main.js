a9ff61d543382af138614149eee468e6899cb4fd
tiago-p-s-d <tiagopsilvad@gmail.com>
2023-10-08 01:28:17 -0300

    testando com gabriel nasa

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import * as THREE from './node_modules/three/build/three.module.js';
 
-window.alert("vc mamou");
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
